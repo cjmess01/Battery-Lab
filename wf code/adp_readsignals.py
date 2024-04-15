29ef2bb0316bc7b93d4977ca127a37e7cb2727cb
cjmess01 <cjmess01@gmail.com>
2024-04-15 12:22:01 -0500

    added my opening calls

--- wf code/adp_readsignals.py
+++ wf code/adp_readsignals.py
@@ -39,10 +39,24 @@ dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0)) # 0 = the device will only be con
 
 
 print("Generating sine wave...")
+# Disables preset configuration, allowing us to configure manually
+dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0)) 
+
+print("Configure and start first analog out channel")
+# Sets it to channel 1 with default carrier node as source. 
+print("A")
 dwf.FDwfAnalogOutNodeEnableSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_int(1))
+# Sets channel 0 to create a sine wave
+print("B")
 dwf.FDwfAnalogOutNodeFunctionSet(hdwf, c_int(0), AnalogOutNodeCarrier, funcSine)
+# Sets frequency of the given function wave
+print("C")
 dwf.FDwfAnalogOutNodeFrequencySet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(1e6))
+# Sets amplitude
+print("D")
 dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(0.9))
+# Starts the instrument
+print("E")
 dwf.FDwfAnalogOutConfigure(hdwf, c_int(0), c_int(1))
 
 
@@ -125,24 +139,6 @@ dwf.FDwfDeviceCloseAll()
 
 
 
-# # Disables preset configuration, allowing us to configure manually
-# dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(0)) 
 
-# print("Configure and start first analog out channel")
-# # Sets it to channel 1 with default carrier node as source. 
-# print("A")
-# dwf.FDwfAnalogOutNodeEnableSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_int(1))
-# # Sets channel 0 to create a sine wave
-# print("B")
-# dwf.FDwfAnalogOutNodeFunctionSet(hdwf, c_int(0), AnalogOutNodeCarrier, funcSine)
-# # Sets frequency of the given function wave
-# print("C")
-# dwf.FDwfAnalogOutNodeFrequencySet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(1e6))
-# # Sets amplitude
-# print("D")
-# dwf.FDwfAnalogOutNodeAmplitudeSet(hdwf, c_int(0), AnalogOutNodeCarrier, c_double(0.9))
-# # Starts the instrument
-# print("E")
-# dwf.FDwfAnalogOutConfigure(hdwf, c_int(0), c_int(1))
 
 
